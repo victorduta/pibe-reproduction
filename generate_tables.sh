9fac73d7ebee574bf6d88dcca7454ccf463363b2
Duta Victor <duta.victor.marin@gmail.com>
2021-01-16 23:25:46 +0100

    Bring artifact tables together.

--- generate_tables.sh
+++ generate_tables.sh
@@ -9,12 +9,12 @@ echo "\begin{tabular}{l|rr}" > paper/tables/test-eval-lmbench-geomeans.tex
 echo "\multicolumn{1}{l|}{Defense}&\multicolumn{1}{l}{LTO}&\multicolumn{1}{l}{PIBE}\\\\" >> paper/tables/test-eval-lmbench-geomeans.tex
 echo "\hline" >> paper/tables/test-eval-lmbench-geomeans.tex
 
-./visualise_results.sh -only-retpolines-test  | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo Retpolines \& | awk '{print $0" \\\\"}' >>  paper/tables/test-eval-lmbench-geomeans.tex
-./visualise_results.sh -baselines test.baseline.noopt -benchmarks  test.+retretpolines.noopt test.+retretpolines.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo Return retpolines \& | awk '{print $0" \\\\"}' >>  paper/tables/test-eval-lmbench-geomeans.tex
+./visualise_results.sh -only-retpolines-test  | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo Retpolines | awk '{print $0" \\\\"}' >>  paper/tables/test-eval-lmbench-geomeans.tex
+./visualise_results.sh -baselines test.baseline.noopt -benchmarks  test.+retretpolines.noopt test.+retretpolines.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo Return retpolines | awk '{print $0" \\\\"}' >>  paper/tables/test-eval-lmbench-geomeans.tex
 
-./visualise_results.sh -baselines test.baseline.noopt -benchmarks  test.+lvi.noopt test.+lvi.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo LVI-CFI \& | awk '{print $0" \\\\"}' >>  paper/tables/test-eval-lmbench-geomeans.tex
+./visualise_results.sh -baselines test.baseline.noopt -benchmarks  test.+lvi.noopt test.+lvi.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo LVI-CFI  | awk '{print $0" \\\\"}' >>  paper/tables/test-eval-lmbench-geomeans.tex
 
-./visualise_results.sh -baselines test.baseline.noopt -benchmarks  test.alldefenses.noopt test.alldefenses.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo All \& | awk '{print $0" \\\\"}' >>  paper/tables/test-eval-lmbench-geomeans.tex
+./visualise_results.sh -baselines test.baseline.noopt -benchmarks  test.alldefenses.noopt test.alldefenses.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo All | awk '{print $0" \\\\"}' >>  paper/tables/test-eval-lmbench-geomeans.tex
 
 echo "\end{tabular}" >> paper/tables/test-eval-lmbench-geomeans.tex
 
@@ -26,14 +26,14 @@ echo "\begin{tabular}{l|rr}" > paper/tables/eval-lmbench-geomeans.tex
 echo "\multicolumn{1}{l|}{Defense}&\multicolumn{1}{l}{LTO}&\multicolumn{1}{l}{PIBE}\\\\" >> paper/tables/eval-lmbench-geomeans.tex
 echo "\hline" >> paper/tables/eval-lmbench-geomeans.tex
 
-./visualise_results.sh -baselines baseline.noopt -benchmarks  baseline.noopt  baseline.pibe-opt.lmbench-work.999900 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo None \& | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
+./visualise_results.sh -baselines baseline.noopt -benchmarks  baseline.noopt  baseline.pibe-opt.lmbench-work.999900 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo None | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
 
-./visualise_results.sh -only-retpolines  | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo Retpolines \& | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
-./visualise_results.sh -baselines baseline.noopt -benchmarks  +retretpolines.noopt +retretpolines.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo Return retpolines \& | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
+./visualise_results.sh -only-retpolines  | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo Retpolines  | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
+./visualise_results.sh -baselines baseline.noopt -benchmarks  +retretpolines.noopt +retretpolines.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo Return retpolines  | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
 
-./visualise_results.sh -baselines baseline.noopt -benchmarks  +lvi.noopt +lvi.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo LVI-CFI \& | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
+./visualise_results.sh -baselines baseline.noopt -benchmarks  +lvi.noopt +lvi.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo LVI-CFI  | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
 
-./visualise_results.sh -baselines baseline.noopt -benchmarks  alldefenses.noopt alldefenses.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print $4}' | xargs echo All \& | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
+./visualise_results.sh -baselines baseline.noopt -benchmarks  alldefenses.noopt alldefenses.pibe-opt.lmbench-work.999999 | grep Geometric |   sed  "s/%/'\\\\'%/"| awk '{print "&"$4}' | xargs echo All  | awk '{print $0" \\\\"}' >>  paper/tables/eval-lmbench-geomeans.tex
 
 echo "\end{tabular}" >> paper/tables/eval-lmbench-geomeans.tex
 
